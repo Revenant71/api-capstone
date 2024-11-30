25d1743f156d8766d2849339fb9de5b34101b91a
Revenant71 <hamo.seno.swu@phinmaed.com>
2024-11-30 14:27:05 +0800

    http response template for tasks and reports

--- tasks.php
+++ tasks.php
@@ -1,4 +1,29 @@
 <?php
+require_once('connectDb.php');
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Content-Type: application/json");
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: GET, POST, PATCH, DELETE");
 
+$db_attempt = new connectDb;
+$db_connection = $db_attempt->connect();
 
+$method = $_SERVER['REQUEST_METHOD'];
+switch ($method) {
+    case 'GET':
+        # code...
+        break;
+
+    case 'POST':
+        # code...
+        break;        
+
+    case 'PATCH':
+        # code...
+        break;
+        
+    case 'DELETE':
+        # code...
+        break;        
+}
 ?>
