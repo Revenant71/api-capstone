0337231a0008659d82a100f6f4833234ed44e25f
Revenant71 <hamo.seno.swu@phinmaed.com>
2024-12-07 12:14:10 +0800

    check account type

---
+++ checkAccountType.php
@@ -0,0 +1,28 @@
+<?php
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Content-Type: application/json");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Credentials: true");
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+switch ($method) {
+    case 'POST':
+        if(!isset($_SESSION['role_user'])){
+            $response = [
+                'statusRole'=>0,
+                'msgRole'=>'CLIENT'
+            ];
+        } else {
+            $response = [
+                'statusRole'=>1,
+                'msgRole'=>'USER'
+            ];
+        }
+        
+    echo json_encode($response);
+    break;
+    
+}
+?>
